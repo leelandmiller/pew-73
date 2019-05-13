95c3136957d17f0dea391ba3dffc066edf9c31ae
Leeland <leelandmiller@gmail.com>
2019-05-13 12:18:22 -0700

    fixing fontSize on photos page

--- src/components/PicturesAddress.js
+++ src/components/PicturesAddress.js
@@ -7,16 +7,26 @@ const PicturesAddress = props => {
   const { pageYOffset } = useScroll();
   const { desktop } = useResize(868);
   let addressStyle = {}
+  let priceStyle = {}
   let contactStyle = {}
   if (pageYOffset > 100 || !desktop) {
     addressStyle = {
-      fontSize: '1.25rem'
+      fontSize: '1.0rem',
+      textAlign: 'left',
+      marginBottom: '4px'
+    }
+    priceStyle = {
+      fontSize: '1.2rem',
+      textAlign: 'left'
     }
     contactStyle = {
       fontSize: '1.1rem'
     }
   } else if (pageYOffset <= 100 && desktop) {
     addressStyle = {
+      fontSize: '2.00rem'
+    }
+    priceStyle = {
       fontSize: '2.00rem'
     }
     contactStyle = {
@@ -35,7 +45,7 @@ const PicturesAddress = props => {
         <h1 className="address" style={addressStyle}>
           1930 W San Marcos Blvd #73, San Marcos, CA 92078
         </h1>
-        <h1 className="address price" style={addressStyle}>
+        <h1 className="address price" style={priceStyle}>
           $319,000
         </h1>
       </div>
