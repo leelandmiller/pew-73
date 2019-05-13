188e913d413efe35d66f0a2dbad55790d12a7e28
Leeland <leelandmiller@gmail.com>
2019-05-12 17:45:48 -0700

    fixing undefined window

--- src/hooks/useListeners.js
+++ src/hooks/useListeners.js
@@ -1,38 +1,42 @@
 import { useState, useEffect } from "react";
 
 export function useScroll() {
-  const [pageYOffset, setPageYOffset] = useState(window.pageYOffset);
+  if (typeof window !== 'undefined') {
+    const [pageYOffset, setPageYOffset] = useState(window.pageYOffset);
 
-  const listener = e => {
-    setPageYOffset(window.pageYOffset)
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', listener);
-    return () => {
-      window.removeEventListener('scroll', listener);
+    const listener = e => {
+      setPageYOffset(window.pageYOffset)
     };
-  }, []);
 
-  return {
-    pageYOffset
-  };
+    useEffect(() => {
+      window.addEventListener('scroll', listener);
+      return () => {
+        window.removeEventListener('scroll', listener);
+      };
+    }, []);
+
+    return {
+      pageYOffset
+    };
+  }
 }
 
 export function useResize(minWidth = 600) {
-  const mq = window.matchMedia(`(min-width: ${minWidth}px)`);
-  const [desktop, setDesktop] = useState(mq.matches);
+  if (typeof window !== 'undefined') {
+    const mq = window.matchMedia(`(min-width: ${minWidth}px)`);
+    const [desktop, setDesktop] = useState(mq.matches);
 
-  const listener = e => {
-    setDesktop(mq.matches)
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', listener);
-    return () => {
-      window.removeEventListener('resize', listener);
+    const listener = e => {
+      setDesktop(mq.matches)
     }
-  }, []);
 
-  return { desktop }
+    useEffect(() => {
+      window.addEventListener('resize', listener);
+      return () => {
+        window.removeEventListener('resize', listener);
+      }
+    }, []);
+
+    return { desktop }
+  }
 }
