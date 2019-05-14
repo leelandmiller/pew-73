8016cf2d549573956d4be125cb905d431f3aee89
Leeland <leelandmiller@gmail.com>
2019-05-13 17:32:43 -0700

    trying to fix hooks

--- src/hooks/useListeners.js
+++ src/hooks/useListeners.js
@@ -1,50 +1,52 @@
 import { useState, useEffect } from "react";
 
 export function useScroll() {
-  if (typeof window !== 'undefined') {
-    const [pageYOffset, setPageYOffset] = useState(window.pageYOffset);
+  const [pageYOffset, setPageYOffset] = useState(0);
 
-    const listener = e => {
-      setPageYOffset(window.pageYOffset)
+  const listener = e => {
+    setPageYOffset(window.pageYOffset)
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listener);
+    return () => {
+      window.removeEventListener('scroll', listener);
     };
+  }, []);
 
-    useEffect(() => {
-      window.addEventListener('scroll', listener);
-      return () => {
-        window.removeEventListener('scroll', listener);
-      };
-    }, []);
-
-    return {
-      pageYOffset
-    };
-  } else {
-    return {
-      pageYOffset: 0
-    }
-  }
+  return {
+    pageYOffset
+  };
+  // if (typeof window !== 'undefined') {
+    
+  // } else {
+  //   return {
+  //     pageYOffset: 0
+  //   }
+  // }
 }
 
 export function useResize(minWidth = 600) {
-  if (typeof window !== 'undefined') {
+  const [desktop, setDesktop] = useState(true);
+  
+  const listener = e => {
     const mq = window.matchMedia(`(min-width: ${minWidth}px)`);
-    const [desktop, setDesktop] = useState(mq.matches);
-
-    const listener = e => {
-      setDesktop(mq.matches)
-    }
-
-    useEffect(() => {
-      window.addEventListener('resize', listener);
-      return () => {
-        window.removeEventListener('resize', listener);
-      }
-    }, []);
-
-    return { desktop }
-  } else {
-    return {
-      desktop: true
-    }
+    setDesktop(mq.matches)
   }
+
+  useEffect(() => {
+    window.addEventListener('resize', listener);
+    return () => {
+      window.removeEventListener('resize', listener);
+    }
+  }, []);
+
+  return { desktop }
+  // if (typeof window !== 'undefined') {
+    
+  // } else {
+  //   return {
+  //     desktop: true
+  //   }
+  // }
 }
