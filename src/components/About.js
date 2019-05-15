2c81d704c4b65d4e0b4f2170b785e0a4f1a9fb89
Leeland <leelandmiller@gmail.com>
2019-05-15 15:05:28 -0700

    fixing typos

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ const About = () => (
 
     <div className="about-content">
       <p className="description">
-        This is home is a beautiful triple wide 2 bedroom, 2 bath situated in a quiet area in the highly coveted Palomar Estates West 55+ community. The view of the hills from the front living room and the front porch are spectacular, and provide a breathtaking perspective of the early morning sunrises. The professionally landscaped exterior is beautiful and low maintenance. The home features dual pane windows throughought the home, with the exception of the sliding glass door. The roof was recently replaced in 2017. The washer and dryer, which are included, were purchased in 2014. The driveway has curved poles and louvers, allocating extra-wide parking space. It has been freshly re-painted with on trend colors and has composite Trex decking for steps in both the front and back entrances to the house. 
+        This home is a beautiful triple wide 2 bedroom, 2 bath situated in a quiet area in the highly coveted Palomar Estates West 55+ community. The view of the hills from the front living room and the front porch are spectacular, and provide a breathtaking perspective of the early morning sunrises. The professionally landscaped exterior is beautiful and low maintenance. The home features dual pane windows throughout the home, with the exception of the sliding glass door. The roof was recently replaced in 2017. The washer and dryer, which are included, were purchased in 2014. The driveway has curved poles and louvers, allocating extra-wide parking space. It has been freshly re-painted with on trend colors and has composite Trex decking for steps in both the front and back entrances to the house. 
       </p>
 
       <div className="about-img-wrapper">
